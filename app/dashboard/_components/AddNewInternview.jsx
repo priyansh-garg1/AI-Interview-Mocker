07993265c2ff26c8a3b465aa270cc1e38d7b1f45
priyansh-garg1 <priyanshgarg382@gmail.com>
2024-06-18 19:59:51 +0530

    Update Interview

--- app/dashboard/_components/AddNewInternview.jsx
+++ app/dashboard/_components/AddNewInternview.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import {
   Dialog,
   DialogContent,
@@ -7,21 +8,134 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { chatSession } from "@/utils/GeminiAIModal";
+import { LoaderCircle } from "lucide-react";
+import { db } from "@/utils/db";
+import { MockInterview } from "@/utils/schema";
+import {v4 as uuid} from 'uuid'
+import { useUser } from "@clerk/nextjs";
+import moment from "moment";
 
 function AddNewInternview() {
+  const [openDialog, setOpenDialog] = useState(false);
+  const [jobPositon, setJobPositon] = useState();
+  const [jobDesc, setJobDesc] = useState();
+  const [jobExperience, setJobExperience] = useState();
+  const [loading, setLoading] = useState(false);
+  const [jsonResponse, setJsonResponse] = useState([])
+  const {user} = useUser();
+
+  const onSubmit = async (e) => {
+    setLoading(true);
+    e.preventDefault();
+    const InputPrompt =
+      "Job position: " +
+      jobPositon +
+      " Job Description: " +
+      jobDesc +
+      " Job Experience: " +
+      jobExperience +
+      " , Depends on Job Positon, Job Description & Years of Experience give us " +
+      process.env.NEXT_PUBLIC_INTERVIEW_QUESTION_COUNT +
+      " Interview questions and Answer in JSON format, Give us question and answer field on JSON";
+
+    const result = await chatSession.sendMessage(InputPrompt);
+    const MockJsonResp = result.response
+      .text()
+      .replace("```json", "")
+      .replace("```", "");
+    console.log(JSON.parse(MockJsonResp));
+    setJsonResponse(MockJsonResp);
+
+    const resp = await db.insert(MockInterview).values({
+      mockId: uuid(),
+      jsonMockResp:MockJsonResp,
+      jobPosition: jobPositon,
+      jobDesc: jobDesc,
+      jobExperience: jobExperience,
+      createdBy: user?.primaryEmailAddress?.emailAddress,
+      createdAt: moment().format("DD-MM-yyyy"),
+    }).returning({mockId:MockInterview.mockId});
+
+    console.log("Inseted DB:",resp);
+
+    setLoading(false);
+  };
   return (
     <div>
-      <div className="p-10 border rounded-lg bg-secondary hover:scale-105 hover:shadow-md cursor-pointer transition-all">
+      <div
+        onClick={() => setOpenDialog(true)}
+        className="p-10 border rounded-lg bg-secondary hover:scale-105 hover:shadow-md cursor-pointer transition-all"
+      >
         <h2 className="font-bold text-lg text-center">+ Add New</h2>
       </div>
-      <Dialog>
+      <Dialog open={openDialog}>
         <DialogTrigger>Open</DialogTrigger>
-        <DialogContent>
+        <DialogContent className="max-w-2xl">
           <DialogHeader>
-            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogTitle className="font-bold text-2xl">
+              Tell us more about your job interviewing
+            </DialogTitle>
             <DialogDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
+              <form onSubmit={onSubmit}>
+                <div className="">
+                  <h2>
+                    Add Details about you job position/role, Job description and
+                    years of experience
+                  </h2>
+                  <div className="mt-7 my-3">
+                    <label htmlFor="">Job Role/Job Position</label>
+                    <Input
+                      placeholder="Ex. Full Stack Developer"
+                      required
+                      onChange={(e) => setJobPositon(e.target.value)}
+                    />
+                  </div>
+                  <div className="my-3">
+                    <label htmlFor="">
+                      Job Description/ Tech Stack (In Short)
+                    </label>
+                    <Textarea
+                      placeholder="Ex. React, Angular, NodeJs, MySql, etc."
+                      required
+                      onChange={(e) => setJobDesc(e.target.value)}
+                    />
+                  </div>
+                  <div className="my-3">
+                    <label htmlFor="">Years of experience</label>
+                    <Input
+                      placeholder="Ex. 5"
+                      type="number"
+                      max="100"
+                      required
+                      onChange={(e) => setJobExperience(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="flex gap-5 justify-end">
+                  <Button
+                    type="button"
+                    variant={"ghost"}
+                    onClick={() => setOpenDialog(false)}
+                  >
+                    Cancel
+                  </Button>
+                  <Button type="submit" disabled={loading}>
+                    {" "}
+                    {loading ? (
+                      <>
+                        <LoaderCircle className="animate-spin" />
+                        {" "}Generating from AI
+                      </>
+                    ) : (
+                      "Start Interview"
+                    )}
+                  </Button>
+                </div>
+              </form>
             </DialogDescription>
           </DialogHeader>
         </DialogContent>
