79dcad980864d02aeda9b5057d23250fdbac7808
priyansh-garg1 <priyanshgarg382@gmail.com>
2024-06-22 23:14:31 +0530

    Update AI Interview

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
   return (
-    <div>hello</div>
+    <div onLoad={()=>router.replace('/dashboard')}></div>
   );
 }
