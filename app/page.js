1ca6e688b65b2f7e1406a91e4547fc5561778988
priyansh-garg1 <priyanshgarg382@gmail.com>
2024-06-24 01:07:52 +0530

    Update Home Page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,19 @@
+'use client'
+import { Button } from "@/components/ui/button";
+import { ArrowRightIcon } from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import Header from "./dashboard/_components/Header";
 
 export default function Home() {
   const router = useRouter();
   return (
-    <div onLoad={()=>router.replace('/dashboard')}></div>
+    <>
+    <Header/>
+    <div className="h-screen w-full flex flex-col gap-10 justify-evenly items-center bg-secondary">
+      <h1 className="font-semibold text-5xl">Your Personal AI Interview Coach</h1>
+      <Button className='flex gap-2' onClick={() => router.push("/dashboard")}>Get Started <ArrowRightIcon /></Button>
+    </div>
+    </>
   );
 }
