632534ea2fcf3005a2aae99e51fbab2b5a1ec083
priyansh-garg1 <priyanshgarg382@gmail.com>
2024-06-25 02:14:07 +0530

    Update HomePage

--- app/page.js
+++ app/page.js
@@ -13,6 +13,7 @@ export default function Home() {
     <div className="h-screen w-full flex flex-col gap-10 justify-evenly items-center bg-secondary">
       <h1 className="font-semibold text-5xl">Your Personal AI Interview Coach</h1>
       <Button className='flex gap-2' onClick={() => router.push("/dashboard")}>Get Started <ArrowRightIcon /></Button>
+      <Button className='flex gap-2' onClick={() => router.push("/jobs")}>Find Jobs</Button>
     </div>
     </>
   );
